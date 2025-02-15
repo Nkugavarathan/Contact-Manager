0346bb416a9e8fd77d387090fcd2be26bc651f34
Nkugavarathan <kugavarathan28@gmail.com>
2025-02-15 17:19:54 +0530

    contactlist

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,23 @@ import AddContact from "./components/AddContact"
 import ContactList from "./components/ContactList"
 
 function App() {
+  const contacts = [
+    {
+      id: "1",
+      name: "varathan",
+      email: "kugavarathan28@gmail.com",
+    },
+    {
+      id: "2",
+      name: "jiro",
+      email: "jiro14@gmail.com",
+    },
+  ]
   return (
     <div className="container">
       <Header />
       <AddContact />
-      <ContactList />
+      <ContactList contacts={contacts} />
     </div>
   )
 }
--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,14 +1,28 @@
 import React from "react"
 
-export default function ContactList() {
+export default function ContactList({ contacts }) {
   return (
     <div>
-      <ul class="list-group mt-3">
-        <li className="list-group-item disabled">Cras justo odio</li>
-        <li className="list-group-item">Dapibus ac facilisis in</li>
-        <li className="list-group-item">Morbi leo risus</li>
-        <li className="list-group-item">Porta ac consectetur ac</li>
-        <li className="list-group-item">Vestibulum at eros</li>
+      <ul className="list-group  mt-3">
+        {contacts.map((contact) => (
+          <li key={contact.id} className="list-group-item">
+            <div>
+              <strong>{contact.name}</strong>
+            </div>
+            <div>{contact.email}</div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-trash "
+              viewBox="0 0 16 16"
+            >
+              <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
+              <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
+            </svg>
+          </li>
+        ))}
       </ul>
     </div>
   )
