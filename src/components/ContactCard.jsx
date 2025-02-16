d15ca2ca9f8a841f6a16bf9ab4b57ac168fac84c
Nkugavarathan <kugavarathan28@gmail.com>
2025-02-16 15:23:20 +0530

    person icon added

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -1,26 +1,40 @@
 import React from "react"
-
+import user from "../images/personicon.png"
 const ContactCard = ({ id, name, email }) => {
   return (
     <div className="list-group">
-      <li class="list-group-item" key={id}>
-        <strong>{name}</strong>
-        <p>{email}</p>
+      {/* style={{ position: "relative" }}*/}
+      <li
+        class="list-group-item"
+        key={id}
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <img
+          src={user}
+          alt="user"
+          // className="img-thumbnail"
+          style={{ borderRadius: 50, height: 100, width: 100 }}
+        />
+        <br />
+        <div>
+          <strong>{name}</strong>
+          <p>{email}</p>
+        </div>
+
         <i
           className="bi bi-trash text-danger"
-          style={{ cursor: "pointer", fontSize: "1.2rem" }}
+          style={{
+            // position: "absolute",
+            cursor: "pointer",
+            fontSize: "1.2rem",
+            // top: 20,
+            // right: 10,
+          }}
         ></i>
-        {/* <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          fill="currentColor"
-          class="bi bi-trash"
-          viewBox="0 0 16 16"
-        >
-          <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
-          <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" /> */}
-        {/* </svg> */}
       </li>
     </div>
   )
